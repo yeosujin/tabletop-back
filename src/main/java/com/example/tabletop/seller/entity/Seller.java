99259c902a24df36c1bb945821b0ff68f94dda7a
gntodtndls156 <gntodtndls156@gmail.com>
2024-07-23 09:51:47 +0900

    feat: add seller entity

--- src/main/java/com/example/tabletop/seller/entity/Seller.java
+++ src/main/java/com/example/tabletop/seller/entity/Seller.java
@@ -1,5 +1,46 @@
 package com.example.tabletop.seller.entity;
 
-public class Seller {
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
 
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@ToString
+@NoArgsConstructor
+public class Seller {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "seller_id", nullable = false)
+    private Long sellerId;
+
+    @Column(name = "login_id", length = 30)
+    private String loginId;
+
+    @Column(name = "username", length = 30)
+    private String username;
+
+    @Column(name = "password")
+    private String password;
+
+    @Column(name = "email", length = 50)
+    private String email;
+
+    @Column(name = "mobile", length = 14)
+    private String mobile;
+
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
+
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+
+    @Column(name = "refresh_token")
+    private String refreshToken;
+
+    @Column(name = "done_click_count_setting")
+    private Boolean doneClickCountSetting;
 }
