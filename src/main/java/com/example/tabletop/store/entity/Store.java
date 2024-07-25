34133833a95f4def91bce64e49c74120f372c618
yeosujin <ysjin9992@gmail.com>
2024-07-25 14:21:41 +0900

    fix : Store Entity

--- src/main/java/com/example/tabletop/store/entity/Store.java
+++ src/main/java/com/example/tabletop/store/entity/Store.java
@@ -3,22 +3,22 @@ package com.example.tabletop.store.entity;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
+import java.util.List;
 import java.util.Set;
 
-import com.example.tabletop.seller.entity.Seller;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
-import jakarta.persistence.CollectionTable;
-import jakarta.persistence.Column;
-import jakarta.persistence.ElementCollection;
-import jakarta.persistence.Entity;
-import jakarta.persistence.EnumType;
-import jakarta.persistence.Enumerated;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
-import jakarta.persistence.Table;
+import com.example.tabletop.image.entity.Image;
+import com.example.tabletop.menu.entity.Menu;
+import com.example.tabletop.order.entity.Order;
+import com.example.tabletop.seller.entity.Seller;
+import com.example.tabletop.store.enums.Day;
+import com.example.tabletop.store.enums.StoreType;
+
+import jakarta.persistence.*;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
@@ -28,6 +28,7 @@ import lombok.ToString;
 @Table(name = "store")
 @ToString
 @Entity
+@EntityListeners(AuditingEntityListener.class)
 public class Store {
 	
 	@Id
@@ -42,7 +43,7 @@ public class Store {
     @Enumerated(EnumType.ORDINAL)
     private StoreType storeType;
 
-    @Column(name = "corporate_registration_number", nullable = true)
+    @Column(name = "corporate_registration_number", nullable = true, unique = true)
     private String corporateRegistrationNumber;
     
     @Column(name = "open_date", nullable = true)
@@ -72,9 +73,11 @@ public class Store {
     @Column(name = "day", nullable = true)
     private Set<Day> holidays;
     
-    @Column(name = "created_at", nullable = false)
+    @CreatedDate
+    @Column(name = "created_at", nullable = false, updatable = false)
     private LocalDateTime createdAt;
 	
+    @LastModifiedDate
     @Column(name = "updated_at", nullable = false)
     private LocalDateTime updatedAt;
 	
@@ -83,18 +86,47 @@ public class Store {
 	@ToString.Exclude
 	private Seller seller;
     
-	public enum StoreType {
-        ORDINARY,
-        TEMPORARY
-    }
-	
-	public enum Day {
-	    MONDAY,
-	    TUESDAY,
-	    WEDNESDAY,
-	    THURSDAY,
-	    FRIDAY,
-	    SATURDAY,
-	    SUNDAY
+    @OneToOne
+    @JoinColumn(name = "image_id")
+    private Image image;
+    
+    @OneToMany(mappedBy = "store", cascade = CascadeType.REMOVE)
+	private List<Menu> menus;
+    
+    @OneToMany(mappedBy = "store", cascade = CascadeType.REMOVE)
+	private List<Order> orders;
+
+    @Builder
+	public Store(Long storeId, String name, StoreType storeType, String corporateRegistrationNumber, LocalDate openDate,
+			LocalDate closeDate, String description, String address, String notice, LocalTime openTime,
+			LocalTime closeTime, Set<Day> holidays, LocalDateTime createdAt, LocalDateTime updatedAt, Seller seller) {
+		this.storeId = storeId;
+		this.name = name;
+		this.storeType = storeType;
+		this.corporateRegistrationNumber = corporateRegistrationNumber;
+		this.openDate = openDate;
+		this.closeDate = closeDate;
+		this.description = description;
+		this.address = address;
+		this.notice = notice;
+		this.openTime = openTime;
+		this.closeTime = closeTime;
+		this.holidays = holidays;
+		this.seller = seller;
 	}
+    
+    public void updateDetails(String name, String description, String address, String notice, LocalTime openTime,
+			LocalTime closeTime, Set<Day> holidays) {
+    	this.name = name;
+		this.description = description;
+		this.address = address;
+		this.notice = notice;
+		this.openTime = openTime;
+		this.closeTime = closeTime;
+		this.holidays = holidays;
+    }
+    
+    public void setImage(Image image) {
+        this.image = image;
+    }
 }
