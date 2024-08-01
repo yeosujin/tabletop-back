e3dc7643b242b02d71f492b4cc86474cf84e1baf
HYH <dihexyldecyl@gmail.com>
2024-08-01 20:13:45 +0900

    #fix: ImageEntity s3 url 필드 추가로 인한 변경

--- src/main/java/com/example/tabletop/store/dto/StoreDetailsDTO.java
+++ src/main/java/com/example/tabletop/store/dto/StoreDetailsDTO.java
@@ -22,15 +22,35 @@ public class StoreDetailsDTO {
     private String notice;
     private LocalTime openTime;
     private LocalTime closeTime;
+<<<<<<< Updated upstream
     private Set<String> holidays;
 	private String sellerName;
 	private String imageBase64;
+=======
+<<<<<<< Updated upstream
+    private Set<Day> holidays;
+	private Seller seller;
+=======
+    private Set<String> holidays;
+	private String sellerName;
+	private String s3Url;
+>>>>>>> Stashed changes
+>>>>>>> Stashed changes
     
     @Builder
     public StoreDetailsDTO(Long storeId, String name, StoreType storeType, String corporateRegistrationNumber,
     		LocalDate openDate, LocalDate closeDate, String description, String address, String notice,
+<<<<<<< Updated upstream
     		LocalTime openTime, LocalTime closeTime, Set<String> holidays, String sellerName, String imageBase64) {
 
+=======
+<<<<<<< Updated upstream
+    		LocalTime openTime, LocalTime closeTime, Set<Day> holidays, Seller seller) {
+    	super();
+=======
+    		LocalTime openTime, LocalTime closeTime, Set<String> holidays, String sellerName, String imageBase64, String s3Url) {
+>>>>>>> Stashed changes
+>>>>>>> Stashed changes
     	this.storeId = storeId;
     	this.name = name;
     	this.storeType = storeType;
@@ -43,8 +63,17 @@ public class StoreDetailsDTO {
     	this.openTime = openTime;
     	this.closeTime = closeTime;
     	this.holidays = holidays;
+<<<<<<< Updated upstream
     	this.sellerName = sellerName;
     	this.imageBase64 = imageBase64;
+=======
+<<<<<<< Updated upstream
+    	this.seller = seller;
+=======
+    	this.sellerName = sellerName;
+    	this.s3Url = s3Url;
+>>>>>>> Stashed changes
+>>>>>>> Stashed changes
     }
     
 }
