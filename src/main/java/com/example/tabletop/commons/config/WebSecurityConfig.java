a27b8d338223d6559413978b6ad38a167dbff6e1
Seong Min <ggbb0308@naver.com>
2024-08-05 18:23:12 +0900

    fix: SecurityConfig modify

--- src/main/java/com/example/tabletop/commons/config/WebSecurityConfig.java
+++ src/main/java/com/example/tabletop/commons/config/WebSecurityConfig.java
@@ -37,17 +37,9 @@ public class WebSecurityConfig {
                 .sessionManagement(sessionManagement -> sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 
                 .authorizeHttpRequests((request) -> request
-                		.requestMatchers(PathRequest.toStaticResources().atCommonLocations()).permitAll() // test
-                		.requestMatchers("/api/sales/*").permitAll()
-                		.requestMatchers("/api/sse/notify/*").permitAll()
-                        .requestMatchers("/api/sellers/signup", "/api/sellers/exists").permitAll()
-                        .requestMatchers("/api/stores/*/menus", "/api/stores/*", "/api/stores/*/details").permitAll()
-                        .requestMatchers("/api/orders/*").permitAll()
-                        .requestMatchers("/api/auth/login", "/api/auth/{loginId}", "/api/auth/token/refresh").permitAll()
-                        .requestMatchers("/api/mail/**", "/api/s3/**", "/api/store/**").permitAll()
-                        // 임시방편으로
-                        .requestMatchers("/api/**").permitAll() // 이렇게는 어떠심
-                        
+                		.requestMatchers(PathRequest.toStaticResources().atCommonLocations()).permitAll()
+                        .requestMatchers("/api/sellers/signup", "/api/sellers/exists").permitAll()                      
+                        .requestMatchers("/api/auth/**", "/api/mail/**").permitAll()                 
                         .anyRequest().authenticated()
                 )
                 
