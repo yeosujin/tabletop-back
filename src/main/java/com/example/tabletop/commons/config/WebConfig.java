49f8d1d15b86aad601fc161481a4d8a0c52c85b3
gntodtndls156 <gntodtndls156@gmail.com>
2024-08-10 12:24:51 +0900

    fix: CORS 수정

--- src/main/java/com/example/tabletop/commons/config/WebConfig.java
+++ src/main/java/com/example/tabletop/commons/config/WebConfig.java
@@ -14,7 +14,7 @@ public class WebConfig implements WebMvcConfigurer {
 	@Override
 	public void addCorsMappings(CorsRegistry registry) {
 		registry.addMapping("/**")
-				.allowedOrigins(allowedOrigins)
+				.allowedOrigins("http://3.37.149.248")
 				.allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
 				.allowedHeaders("*")
 				.allowCredentials(true)
