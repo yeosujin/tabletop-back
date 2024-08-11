d2f86ee5e8e17334853500248ad9f6a5689a54bd
gntodtndls156 <gntodtndls156@gmail.com>
2024-08-11 16:55:48 +0900

    fix: add cors localhost

--- src/main/java/com/example/tabletop/commons/config/WebConfig.java
+++ src/main/java/com/example/tabletop/commons/config/WebConfig.java
@@ -11,7 +11,7 @@ public class WebConfig implements WebMvcConfigurer {
 	@Override
 	public void addCorsMappings(CorsRegistry registry) {
 		registry.addMapping("/api/**")
-				.allowedOrigins("http://3.37.149.248", "http://tabletop-magicpos.site")
+				.allowedOrigins("http://3.37.149.248", "http://tabletop-magicpos.site", "http://localhost:3000")
 				.allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
 				.allowedHeaders("*")
 				.allowCredentials(true)
